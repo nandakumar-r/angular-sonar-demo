5b38388e40735722822585b1c58f2c89bc52135c
nandakumarr <nandakumar.r@perfomatix.com>
2020-06-20 22:53:48 +0530

    removed lint error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'angular-sonar-demo-app';
-  var {a: {}} = foo;
+  title = "angular-sonar-demo-app";
 }
